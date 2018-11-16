fb3eb865c1654f82e8c69592d74346f345ba5d5f
maguillens <44014308+maguillens@users.noreply.github.com>
2018-11-16 02:39:38 -0500

    Add files via upload

---
+++ Project1_Plot2.R
@@ -0,0 +1,36 @@
+setwd("C:/Users/MIGUEL GUILLEN/Documents/GitHub/ExData_Plotting1/Project1")
+
+hpc <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+## Format date to Type Date
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+hpc <- subset(hpc,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+## Remove incomplete observation
+hpc <- hpc[complete.cases(hpc),]
+
+## Combine Date and Time column
+dateTime <- paste(hpc$Date, hpc$Time)
+
+## Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+## Remove Date and Time column
+hpc <- hpc[ ,!(names(hpc) %in% c("Date","Time"))]
+
+## Add DateTime column
+hpc <- cbind(dateTime, hpc)
+
+## Format dateTime Column
+hpc$dateTime <- as.POSIXct(dateTime)
+
+
+## Create Plot 2
+dev.copy(png,"plot2.png", width=480, height=480)
+plot(hpc$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.off()
+
+
+
---
+++ Project1_Plot3.R
@@ -0,0 +1,39 @@
+setwd("C:/Users/MIGUEL GUILLEN/Documents/GitHub/ExData_Plotting1/Project1")
+
+hpc <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+## Format date to Type Date
+hpc$Date <- as.Date(hpc$Date, "%d/%m/%Y")
+
+## Filter data set from Feb. 1, 2007 to Feb. 2, 2007
+hpc <- subset(hpc,Date >= as.Date("2007-2-1") & Date <= as.Date("2007-2-2"))
+
+## Remove incomplete observation
+hpc <- hpc[complete.cases(hpc),]
+
+## Combine Date and Time column
+dateTime <- paste(hpc$Date, hpc$Time)
+
+## Name the vector
+dateTime <- setNames(dateTime, "DateTime")
+
+## Remove Date and Time column
+hpc <- hpc[ ,!(names(hpc) %in% c("Date","Time"))]
+
+## Add DateTime column
+hpc <- cbind(dateTime, hpc)
+
+## Format dateTime Column
+hpc$dateTime <- as.POSIXct(dateTime)
+
+## Create Plot 3
+dev.copy(png,"plot3.png", width=480, height=480)
+with(hpc, {
+  plot(Sub_metering_1~dateTime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~dateTime,col='Red')
+  lines(Sub_metering_3~dateTime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
